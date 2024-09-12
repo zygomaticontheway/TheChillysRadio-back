b0ceec78867210e6508e657216efb35eb3689dfb
VladVel24 <vlad.velykyy@gmail.com>
2024-09-12 10:24:08 +0200

    add methods updateUser and changePassword in the Controller

--- src/main/java/theChillys/chillys_radio/user/dto/ChangePasswordDto.java
+++ src/main/java/theChillys/chillys_radio/user/dto/ChangePasswordDto.java
@@ -1,5 +1,15 @@
 package theChillys.chillys_radio.user.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ChangePasswordDto {
     private String newPassword;
 }
