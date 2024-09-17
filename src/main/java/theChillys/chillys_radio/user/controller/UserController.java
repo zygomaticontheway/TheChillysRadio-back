6be446a1d6cb27cd832338f31a68e7841d4c5195
Valerii Kuchynskyi <valeryk@hotmail.com>
2024-09-17 12:37:00 +0200

    add station to favorite, fix my profile

--- src/main/java/theChillys/chillys_radio/user/controller/UserController.java
+++ src/main/java/theChillys/chillys_radio/user/controller/UserController.java
@@ -5,18 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
-import theChillys.chillys_radio.station.dto.StationResponseDto;
 import theChillys.chillys_radio.user.dto.ChangePasswordDto;
-
 import theChillys.chillys_radio.user.dto.UserRequestDto;
 import theChillys.chillys_radio.user.dto.UserResponseDto;
 import theChillys.chillys_radio.user.service.IUserService;
 
-import java.util.Optional;
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -43,16 +39,16 @@ public class UserController {
     public UserResponseDto createUser(@RequestBody UserRequestDto dto) {
         return service.createUser(dto);
     }
-  
+
     @GetMapping("/users/{id}")
-     public Optional<UserResponseDto> getUserById(@PathVariable(name="id") Long userId) {
+    public Optional<UserResponseDto> getUserById(@PathVariable(name = "id") Long userId) {
         return service.getUserById(userId);
     }
-  
-     @PostMapping("/users/my-favorites")
-     public boolean toggleFavoriteStation(@RequestParam Long id, @RequestParam String stationuuid) {
-         return service.toggleFavoriteStation(id, stationuuid);
-     }
+
+    @PostMapping("/users/my-favorites")
+    public boolean toggleFavoriteStation(@RequestParam Long id, @RequestParam String stationuuid) {
+        return service.toggleFavoriteStation(id, stationuuid);
+    }
 
     @PutMapping("/users/{id}")
     public UserResponseDto updateUser(@PathVariable(name = "id") Long Id, @RequestBody UserRequestDto dto) {
@@ -64,7 +60,7 @@ public class UserController {
         return service.changePassword(userId, passwordDto.getNewPassword());
 
     }
-  
+
     @PreAuthorize("hasRole('ADMIN')")  // only for admin
     @PutMapping("/set-admin/{name}")
     public UserResponseDto setAdminRole(@PathVariable(name = "name") String name) {
